54d1e7357939fa4bd371941f916d5726ee25ae5f
Muhammad Ahmed Ashar <hafizmahmedashar@gmail.com>
2025-06-02 19:59:56 +0500

    checkout page

--- frontend/src/pages/Checkout/Checkout.jsx
+++ frontend/src/pages/Checkout/Checkout.jsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
+import stripe from "../../assets/stripe_logo.png";
+import CartTotal from "../../components/CartTotal/CartTotal";
+import "./Checkout.css";
 
 const Checkout = () => {
+  const [method, setMethod] = useState("cod");
   return (
-    <div>Checkout</div>
-  )
-}
+    <div>
+      <form action="" className="form-container">
+        <div className="form-left">
+          <fieldset className="payment-method">
+            <legend>Payment Options</legend>
+            <div className="payment-options">
+              <div
+                onClick={() => setMethod("stripe")}
+                className={`payment-option ${
+                  method === "stripe" ? "selected" : ""
+                }`}
+              >
+                <img src={stripe} alt="" className="payment-logo" />
+              </div>
+              <div
+                onClick={() => setMethod("cod")}
+                className={`payment-option ${
+                  method === "cod" ? "selected" : ""
+                }`}
+              >
+                <span className="payment-text">CASH ON DELIVERY</span>
+              </div>
+            </div>
+          </fieldset>
 
-export default Checkout
+          <div className="form-title">
+            <h2>Shipping Address</h2>
+          </div>
+          <div className="form-row">
+            <input
+              type="text"
+              className="form-input"
+              placeholder="First Name"
+            />
+            <input type="text" className="form-input" placeholder="Last Name" />
+          </div>
+          <input
+            type="email"
+            className="form-input"
+            placeholder="Email Address"
+          />
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Phone Number"
+          />
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Street Address"
+          />
+          <div className="form-row">
+            <input type="text" className="form-input" placeholder="City" />
+            <input type="text" className="form-input" placeholder="State" />
+          </div>
+          <div className="form-row">
+            <input type="text" className="form-input" placeholder="Zipcode" />
+            <input type="text" className="form-input" placeholder="Country" />
+          </div>
+        </div>
+
+        <div className="form-right">
+          <CartTotal />
+          <div className="form-submit">
+            <button type="submit" className="submit-button">
+              PLACE ORDER
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Checkout;
