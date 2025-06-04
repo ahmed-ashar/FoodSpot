8f8b04db3516acfee6143e071cef2b22e00e0cc3
Muhammad Ahmed Ashar <hafizmahmedashar@gmail.com>
2025-06-04 20:46:29 +0500

    today's first commit

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Login.css'
 
 const Login = () => {
+
+  const [currentState, setCurrentState] = useState('Login')
   return (
-    <div>Login</div>
+    <div>
+      <form action="" className="auth-form">
+        <div className="form-header">
+          <p className='form-title'>{currentState} </p>
+        </div>
+        {
+          currentState === 'Login' ? null:(
+        <input type="text" className='form-input' placeholder='Full Name' required />
+
+          )
+        }
+        <input type="email" className='form-input' placeholder='Email' required />
+        <input type="password" className='form-input' placeholder='Password' required />
+        <div className="form-footer">
+          <p className='fgt-password'>Forget Password ?</p>
+          {
+            currentState === 'Login' ? (
+              <p onClick={() => setCurrentState('Sign Up')} className='toggle-auth-state'>Create Account</p>
+            ) : (
+          
+          <p onClick={() => setCurrentState('Login')} className='toggle-auth-state'>Login Here</p>
+            )
+          }
+        </div>
+        <button className="form-button">
+          {currentState === 'Login' ? 'Sign In' : 'Sign Up'}
+        </button>
+      </form>
+    </div>
   )
 }
 
