d5db7b7f75b6f18f4af69463e359926a5e0c12cf
Ahmed Ashar <155929233+ahmed-ashar@users.noreply.github.com>
2025-06-18 21:26:20 +0500

    Update Order.jsx

--- frontend/src/pages/Order/Order.jsx
+++ frontend/src/pages/Order/Order.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FoodContext } from "../../context/FoodContext";
 import axios from "axios";
 import { backendUrl } from "../../App";
